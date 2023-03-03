7eec3b7450d88483a18590f73bd349f39856f4cc
honzikhk <chudicekjan@gmail.com>
2023-03-03 23:28:24 +0100

    iterate in homepage thrue object_list

--- cryptocurrencies/models.py
+++ cryptocurrencies/models.py
@@ -8,3 +8,6 @@ class CryptoCurrency(models.Model):
     saved_on = models.CharField(max_length=30, default="very safe place")
     note = models.CharField(null=True, blank=True, max_length=255)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name        # represent objects by their names in admin page
--- cryptocurrencies/views.py
+++ cryptocurrencies/views.py
@@ -1,18 +1,11 @@
-from django.http import HttpResponse
 from django.views.generic import ListView
 from .models import CryptoCurrency
 
 
 class CryptocurrenciesHomepageView(ListView):
+    model = CryptoCurrency
     template_name = "cryptocurrencies/homepage.html"
 
-    def get_queryset(self):
-        return CryptoCurrency.objects.all()     # později přidat .filter(user=self.request.user)
 
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(CryptocurrenciesHomepageView, self).get_context_data(*args, **kwargs)
-    #     extra_content = {
-
-    #     }
-    #     context.update(extra_content)
-    #     return context
+def get_price_per_coin(coin):
+    return price
